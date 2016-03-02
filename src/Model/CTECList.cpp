1529def30254dfba0db9479b8a5419c372f54d43
Sam Montoya <smon7958@740s-pl-118.local>
2016-03-02 11:52:00 -0700

    Added the List Classes

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -1,34 +1,89 @@
-/*
- * CTECList.cpp
- *
- *  Created on: Feb 22, 2016
- *      Author: smon7958
- */
-
 #include "CTECList.h"
 
 template<class Type>
-CTECList<Type>::CTECList() {
-	this -> size = 0;
-	this -> head = nullptr;
-	this -> end = nullptr;
-
-}
-
-template<class Type>
-CTECList<Type>::~CTECList() {
-
-}
-
-template<class Type>
-Type CTECList<Ty> :: removeFromFont()
+CTECList<Type>::CTECList()
 {
-	//Find the next spot
-	ArrayNode<Type> * newHead = new ArrayNode<Type>();
-	newHead = head->getNext();
-	//Remove head
-	delete head;
-	//Move head to next spot
-	head = new Head;
+	this->size = 0;
+	this->head = nullptr;
+	this->end = nullptr;
+
 }
 
+template<class Type>
+CTECList<Type>::~CTECList()
+{
+	// TODO Auto-generated destructor stub
+}
+
+template<class Type>
+Type CTECList<Type> :: removeFromFront()
+{
+	assert(this->size > 0);
+
+	Type thingToRemove;
+	ArrayNode<Type> * newHead = new ArrayNode<Type>();
+	newHead = this->head->getNext();
+	thingToRemove = this->head->getValue();
+	this->head = newHead;
+
+	size--;
+
+	return thingToRemove;
+}
+
+template<class Type>
+Type CTECList<Type> :: removeFromEnd()
+{
+
+}
+
+template<class Type>
+Type CTECList<Type> :: removeFromIndex(int index)
+{
+	assert(this->size > 0);
+	assert(index >= 0 && index < size);
+
+	Type thingToRemove;
+
+	//Array<Type> * previous, deleteMe, newNext;
+
+	if(index == 0)
+	{
+		thingToRemove = removeFromFront();
+	}
+	else if(index == size-1)
+	{
+		thingToRemove = removeFromEnd();
+	}
+	else
+	{
+		for(int spot =0; spot < index+1; spot++)
+		{
+
+		}
+	}
+
+}
+
+template<class Type>
+	void CTECList<Type> :: calculateSize()
+	{
+		assert(size >= 0);
+
+		int count = 0;
+		if(head == nullptr)
+		{
+			size = count;
+		}
+		else
+		{
+			count++;
+			ArrayNode<Type> * current = head;
+			while(current->getNext() !=nullptr)
+			{
+				count++;
+				current = current->getNext();
+			}
+			size = count;
+		}
+}
