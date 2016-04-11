df1e0cee89c44d8a925394d09346f6a689c33eb2
Sam Montoya <smon7958@740s-pl-118.local>
2016-04-11 12:16:11 -0600

    Added the TreeNode.H

---
+++ src/Model/TreeNode.cpp
@@ -0,0 +1,18 @@
+/*
+ * TreeNode.cpp
+ *
+ *  Created on: Apr 11, 2016
+ *      Author: smon7958
+ */
+
+#include "TreeNode.h"
+
+TreeNode::TreeNode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TreeNode::~TreeNode() {
+	// TODO Auto-generated destructor stub
+}
+
